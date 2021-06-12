10eb81607751fad79a77aedf3ba42114f73ffcb8
Gerontakos <tgis@uw.edu>
2021-06-11 18:49:28 -0700

    modify ttl-2-nt-json.py so it overwrites already existing files

--- ttl-2-nt-json.py
+++ ttl-2-nt-json.py
@@ -1,4 +1,5 @@
 import rdflib
+import os
 
 g = rdflib.Graph()
 
@@ -7,10 +8,16 @@ turtle = g.parse("../uwlswd_vocabs/newspaperGenreList.ttl", format="turtle")
 json = turtle.serialize(format='json-ld')
 nt = turtle.serialize(format='nt')
 
+if os.path.exists('../uwlswd_vocabs/newspaperGenreList.json'):
+    os.remove('../uwlswd_vocabs/newspaperGenreList.json')
+
+if os.path.exists('../uwlswd_vocabs/newspaperGenreList.nt'):
+    os.remove('../uwlswd_vocabs/newspaperGenreList.nt')
+
 file = open('../uwlswd_vocabs/newspaperGenreList.json', 'wb')
 file.write(json)
 file.close()
 
-file = open('../uwlswd_vocabs/ngl.newspaperGenreList', 'wb')
+file = open('../uwlswd_vocabs/newspaperGenreList.nt', 'wb')
 file.write(nt)
 file.close()
