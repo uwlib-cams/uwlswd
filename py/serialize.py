755b0a886cb78b93d8b70daebf8f61552734100f
briesenberg07 <ries07@uw.edu>
2023-02-17 20:35:46 -0800

    outline plan for a script to serialize See #12

---
+++ py/serialize.py
@@ -0,0 +1,4 @@
+# TO DO: Write a proper tool for serializing and re-serializing files
+    # get file name from user input
+    # determine source serialization from user input
+    # parse source file, output remaining serializations
