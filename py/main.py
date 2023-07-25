0a45a543378b5457d2de7ac691c18c9be16ef460
paynec4 <48925631+cspayne@users.noreply.github.com>
2023-07-24 17:56:00 -0700

    Update main.py
    issue #18

--- py/main.py
+++ py/main.py
@@ -1,3 +1,5 @@
+# main file for workflow/management of uwlswd datasets/vocabs
+
 from textwrap import dedent
 import rdflib
 import json
@@ -11,6 +13,10 @@ from serialize import serialize
 # serialize formats
 # call xsl html 
 
+# this script should be run from the top-level folder of uwlswd
+# saxon should be installed in top-level directory
+
+# check set-up
 print(dedent("""Please confirm:
 1) Terminal is open in the uwlswd top-level directory
 2) Saxon processor .jar file is located in the user's home (~) directory"""))
@@ -31,9 +37,8 @@ For example: '11.1', '11.4', etc.
 > """)
 saxon_version = input(saxon_version_prompt)
 
-
+# get file path
 def prompt_user(): 
-    # get file path
     file_prompt = dedent("""Enter the path of the file relative to the working directory. 
     The file must have the extenstion ".rdf", ".ttl", ".jsonld", or ".nt"
     For example: '../uwlswd_vocabs/newspaper_genre_list.ttl'
@@ -52,8 +57,8 @@ def prompt_user():
         else:
             exit(0)
 
+# process file path for separate variables 
 file_path = prompt_user()
-
 split = file_path.split("/")
 
 directory = ""
@@ -63,36 +68,43 @@ while len(split) > 1:
 split = split[0].split(".")
 format = split.pop()
 file_name = split.pop()
-output_file = f'{directory}{file_name}.html'
-# print("format: " + format)
-# print("file_name: " + file_name)
+#output_file = f'{directory}{file_name}.html'
+output_file = "test_html.xsl"
 
 if format not in ["jsonld","rdf","ttl","nt"]:
     print("Error: file is not one of the accepted formats")
     exit(0)
 
+# check existence/accuracy of metadata for html+rdfa
 prompt_md(directory, file_name)
 
+# serialize 
 print(dedent(f"""{'=' * 20}
 SERIALIZING DATA
 {'=' * 20}"""))
 
 serialize(format, file_path, directory, file_name)
 
-# print(dedent(f"""{'=' * 20}
-# GENERATING HTML+RDFa
-# {'=' * 20}"""))
+# generate html+rdfa
+print(dedent(f"""{'=' * 20}
+GENERATING HTML+RDFa
+{'=' * 20}"""))
 
-# rdf2rdfa_stylesheet = "xsl/rdf2rdfa-draft.xsl"
-# os_command = f"""java -cp ~/{saxon_dir}/saxon-he-{saxon_version}.jar 
-# net.sf.saxon.Transform -t 
-# -s:{file_path} 
-# -xsl:{rdf2rdfa_stylesheet}
-# -o:{output_file}"""
+if (file_path.endswith('.rdf')):
+    xsl_input_file = file_path
+else:
+    xsl_input_file = file_path.rsplit(".", 1)[0] + ".rdf"
 
-# os_command = os_command.replace('\n', '')
-# os.system(os_command)
+rdf2rdfa_stylesheet = "xsl/rdf2rdfa-draft.xsl"
+os_command = f"""java -cp ~/{saxon_dir}/saxon-he-{saxon_version}.jar 
+net.sf.saxon.Transform -t 
+-s:{file_path} 
+-xsl:{rdf2rdfa_stylesheet} 
+-o:{output_file}"""
 
-# print(dedent(f"""{'=' * 20}
-# COMPLETE
-# {'=' * 20}"""))
+os_command = os_command.replace('\n', '')
+os.system(os_command)
+
+print(dedent(f"""{'=' * 20}
+COMPLETE
+{'=' * 20}"""))
