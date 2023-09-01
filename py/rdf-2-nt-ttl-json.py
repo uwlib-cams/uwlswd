cc5ca817574ae02f0ead040d82fa6b9b088fb23d
Gerontakos <tgis@uw.edu>
2023-08-31 19:15:23 -0700

    add 29 Datacite files for 008 vocabularies; edit rdf2rdfa for 008

--- py/rdf-2-nt-ttl-json.py
+++ py/rdf-2-nt-ttl-json.py
@@ -2,21 +2,21 @@ import rdflib
 
 g = rdflib.Graph()
 
-xml = g.parse("../../uwlswd_datasets/void_description_of_the_dataset_university_of_washington_libraries_semantic_web_data.rdf", format="xml")
+xml = g.parse("../../uwlswd_vocabs_marc_006_008/008/continuing_form_of_original_item/continuing_form_of_original_item.rdf", format="xml")
 
 
 nt = xml.serialize(format='nt')
 turtle = xml.serialize(format='turtle')
 json = xml.serialize(format='json-ld')
 
-file = open('../../uwlswd_datasets/void_description_of_the_dataset_university_of_washington_libraries_semantic_web_data.nt', 'w')
+file = open('../../uwlswd_vocabs_marc_006_008/008/continuing_form_of_original_item/continuing_form_of_original_item.nt', 'w')
 file.write(nt)
 file.close()
 
-file = open('../../uwlswd_datasets/void_description_of_the_dataset_university_of_washington_libraries_semantic_web_data.ttl', 'w')
+file = open('../../uwlswd_vocabs_marc_006_008/008/continuing_form_of_original_item/continuing_form_of_original_item.ttl', 'w')
 file.write(turtle)
 file.close()
 
-file = open('../../uwlswd_datasets/void_description_of_the_dataset_university_of_washington_libraries_semantic_web_data.json', 'w')
+file = open('../../uwlswd_vocabs_marc_006_008/008/continuing_form_of_original_item/continuing_form_of_original_item.json', 'w')
 file.write(json)
 file.close()
